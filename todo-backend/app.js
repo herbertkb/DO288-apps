aacb6342f89c2c9eebaa9b6ad06d06bb331b87b1
Student User <student@workstation.lab.example.com>
2020-01-23 21:44:18 +0000

    uncomment exception handler

--- todo-backend/app.js
+++ todo-backend/app.js
@@ -28,9 +28,9 @@ server.listen(port, function (err) {
 });
 
 
-/* 
+ 
 process.on('uncaughtException', function (err) {
     console.error(JSON.parse(JSON.stringify(err, ['stack', 'message', 'inner'], 2)))
 });
-*/    
+    
 
